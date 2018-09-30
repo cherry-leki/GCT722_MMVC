4bc3ede6f979f1be7f633cb7e5dcb2f0b6154ade
CL <chaelin.kim@kaist.ac.kr>
2018-09-30 14:35:27 +0900

    add comment in RANSAC

--- HW1/MathMethods18-Ex01-20183151-Chaelin-Kim/code/main_RANSAC.m
+++ HW1/MathMethods18-Ex01-20183151-Chaelin-Kim/code/main_RANSAC.m
@@ -1,9 +1,9 @@
 clear all
 
 %% Initial settings
-n = 100;
-center = [0, 0];
-radius = 5;
+n = 100;                % Number of data points
+center = [0, 0];        % Center value for the synthesized circle
+radius = 5;             % Radius value for the synthesized circle
 inlierThreshold = 0.1;
 outlierRatio = [0.05, 0.2, 0.3, 0.7];
 
@@ -19,6 +19,7 @@ end
 
 % Execute exhaustive search
 for itr = 1:size(outlierRatio, 2)
+    % The result is shown on the output window
     doExhaustiveSearch(data{itr}, 3, inlierThreshold, outlierRatio(itr));
 end
     
