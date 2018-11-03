0b1a03fe18738cb7c08d5fd7ca1bc04b1fce51a2
CL <chaelin.kim@kaist.ac.kr>
2018-11-03 13:39:47 +0900

    Update HW3/MathMethods18-Ex03-20183151-Chaelin-Kim/MathMethods18-Ex03-20183151-Chaelin-Kim/code/main.m

--- HW3/MathMethods18-Ex03-20183151-Chaelin-Kim/MathMethods18-Ex03-20183151-Chaelin-Kim/code/main.m
+++ HW3/MathMethods18-Ex03-20183151-Chaelin-Kim/MathMethods18-Ex03-20183151-Chaelin-Kim/code/main.m
@@ -67,7 +67,8 @@ while 1
     
     % The iterations stop when the lower and upper bound are nearer than 1,
     % because they will lead to the same number of inliers
-    if upperBound - lowerBound <= 1
+    if (spaceList{1, 2} == upperBound && spaceList{1, 1} == lowerBound) && ...
+        (spaceList{1, 2} - spaceList{1, 1} <= 1)
         finalSpace = spaceList(1, :);
         break;
     end
