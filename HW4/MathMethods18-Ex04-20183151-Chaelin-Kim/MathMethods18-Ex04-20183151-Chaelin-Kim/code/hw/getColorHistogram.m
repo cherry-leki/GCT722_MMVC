ced00575b15303048cfb0e6f74955492ab3b31c5
CL <chaelin.kim@kaist.ac.kr>
2018-11-24 19:41:59 +0900

    change variable name

--- HW4/MathMethods18-Ex04-20183151-Chaelin-Kim/MathMethods18-Ex04-20183151-Chaelin-Kim/code/hw/getColorHistogram.m
+++ HW4/MathMethods18-Ex04-20183151-Chaelin-Kim/MathMethods18-Ex04-20183151-Chaelin-Kim/code/hw/getColorHistogram.m
@@ -22,20 +22,20 @@ end
 
 numColorValue = 256;
 step = numColorValue / histRes;
-rgbIntensity = zeros(size(seed,1), 3, 10);
+rgb_Intens = zeros(size(seed,1), 3, histRes);
 for i=1:histRes
-    rgbIntensity(:,1,i) = seed_rgb(:,1) >= step * (i-1) & seed_rgb(:,1) < step * i;
-    rgbIntensity(:,2,i) = seed_rgb(:,2) >= step * (i-1) & seed_rgb(:,2) < step * i;
-    rgbIntensity(:,3,i) = seed_rgb(:,3) >= step * (i-1) & seed_rgb(:,3) < step * i;
+    rgb_Intens(:,1,i) = seed_rgb(:,1) >= step * (i-1) & seed_rgb(:,1) < step * i;
+    rgb_Intens(:,2,i) = seed_rgb(:,2) >= step * (i-1) & seed_rgb(:,2) < step * i;
+    rgb_Intens(:,3,i) = seed_rgb(:,3) >= step * (i-1) & seed_rgb(:,3) < step * i;
 end
 
 % Create the histogram
 for r=1:histRes
     for g=1:histRes
         for b=1:histRes
-            hist(r,g,b) = nnz(rgbIntensity(:,1,r) & ...
-                              rgbIntensity(:,2,g) & ...
-                              rgbIntensity(:,3,b));
+            hist(r,g,b) = nnz(rgb_Intens(:,1,r) & ...
+                              rgb_Intens(:,2,g) & ...
+                              rgb_Intens(:,3,b));
         end
     end
 end
