95a5ac3ecaf4ebda720e55e2915bb8857d12597e
CL <chaelin.kim@kaist.ac.kr>
2018-11-26 02:47:49 +0900

    change the variable name

--- HW4/MathMethods18-Ex04-20183151-Chaelin-Kim/MathMethods18-Ex04-20183151-Chaelin-Kim/code/GraphCut/handleMaxFlow_t01.m
+++ HW4/MathMethods18-Ex04-20183151-Chaelin-Kim/MathMethods18-Ex04-20183151-Chaelin-Kim/code/GraphCut/handleMaxFlow_t01.m
@@ -6,15 +6,13 @@ clear all;
 unary = [9 7 5;
          4 7 8];
 % pair: i j e00 e01 e10 e11
-% pair = [1 2 0 2 3 0;
-%         2 3 0 1 5 0; ];
 pair = [1 2 0 2 3 0;
         2 3 0 1 5 0; ];
 
 h = BK_Create(3);
 BK_SetUnary(h, unary);
 BK_SetPairwise(h, pair);
-e = BK_Minimize(h);
+cutCost = BK_Minimize(h);
 labeling = BK_GetLabeling(h);
 
 BK_Delete(h);
